c03326a916bc3274c2dcea91d076ddf4db88bce9
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-10-16 02:57:37 +0100

    Add cli create command

---
+++ src/commands/create.ts
@@ -0,0 +1,58 @@
+import { Command, flags } from '@oclif/command';
+import chalk from 'chalk';
+import { log } from '../libs/display';
+import { prompt } from 'enquirer';
+
+import { ProjectGenerator, ProjectGeneratorOptions } from '../libs/ProjectGenerator';
+
+export default class Create extends Command {
+  static description = 'Creates a new Express project.';
+
+  static aliases = ['c'];
+
+  async run() {
+    const question = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your project name',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter your project description',
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Enter your project author',
+      },
+      {
+        type: 'input',
+        name: 'repository',
+        message: 'Enter you project repository',
+      },
+      {
+        type: 'select',
+        name: 'skipInstall',
+        message: 'Skip package installation?',
+        choices: ['yes', 'no'],
+      },
+    ];
+
+    const answers = await prompt<ProjectGeneratorOptions>(question);
+    const generator = new ProjectGenerator(answers);
+
+    try {
+      this.log('\n' + log.withBrand('Hang tight while we set up your new Express app!') + '\n');
+      await generator.run();
+
+      this.log('\n' + log.withBrand('Your new Express app is ready! Next steps:') + '\n');
+      this.log('- Go inside your project folder');
+      this.log(log.withCaret(chalk.bold.blue(`cd ${answers.name}`)));
+      this.log(`- Open ${chalk.bold.blue('README.md')} file and follow the getting started guide.`);
+    } catch (error) {
+      this.error(error);
+    }
+  }
+}
---
+++ src/libs/ProjectGenerator.ts
@@ -0,0 +1,126 @@
+import spawn from 'cross-spawn';
+import { log } from './display';
+import * as path from 'path';
+import { existsSync, mkdirSync } from 'fs';
+import { readJsonSync, writeJsonSync } from 'fs-extra';
+import chalk from 'chalk';
+
+export interface ProjectGeneratorOptions {
+  destinationRoot: string;
+  name: string;
+  description: string;
+  author: string;
+  skipInstall: boolean | string;
+  repository: string;
+}
+
+export class ProjectGenerator {
+  constructor(private options: ProjectGeneratorOptions) {
+    this.options.destinationRoot = path.join(this.options.name!);
+    this.options.skipInstall = this.options.skipInstall === 'yes';
+  }
+
+  createProjectDir() {
+    if (!existsSync(this.options.destinationRoot)) mkdirSync(this.options.destinationRoot);
+  }
+
+  async cloneRepo() {
+    const cloningSpinner = log.spinner(log.withBrand('Setting up project')).start();
+
+    spawn.sync(
+      'git',
+      ['clone', 'https://github.com/danielcoker/node-express-boilerplate.git', '.'],
+      { stdio: 'ignore' },
+    );
+
+    // Remove .git
+    spawn.sync('rm', ['-rf', '.git']);
+
+    cloningSpinner.succeed();
+  }
+
+  async updatePackageInfo() {
+    const packageFile = path.join('package.json');
+
+    try {
+      const pkg = readJsonSync(packageFile);
+
+      pkg.name = this.options.name;
+      pkg.repository.type = undefined;
+      pkg.repository.url = this.options.repository;
+      pkg.author = this.options.author;
+      pkg.description = this.options.description;
+
+      pkg.bugs = undefined;
+      pkg.homepage = undefined;
+
+      await writeJsonSync(packageFile, pkg, { spaces: 2 });
+
+      return true;
+    } catch (error) {
+      console.error(error);
+      log.error('Unable to read package.json');
+
+      return false;
+    }
+  }
+
+  async postWrite() {
+    const gitInitResult = spawn.sync('git', ['init'], { stdio: 'ignore' });
+
+    await this.updatePackageInfo();
+
+    if (!this.options.skipInstall) {
+      const installingSpinner = log.spinner(log.withBrand('Installing dependencies')).start();
+
+      // Run `npm install`
+      const installPackageCommand = spawn.sync('npm', ['install']);
+
+      if (installPackageCommand.status === 0) {
+        installingSpinner.succeed();
+      } else {
+        installingSpinner.fail(
+          chalk.red.bold(
+            "We had some trouble connecting to the network. We'll skip installing dependencies now. Make sure to run `npm install` once you're connected again.",
+          ),
+        );
+      }
+    }
+
+    if (gitInitResult.status === 0) {
+      this.commitChanges();
+    } else {
+      log.warning('Failed to run git init.');
+      log.warning('Find out more about how to install git here: https://git-scm.com/downloads.');
+    }
+  }
+
+  commitChanges() {
+    const commands: Array<[string, string[], object]> = [
+      ['git', ['add', '.'], { stdio: 'ignore' }],
+      ['git', ['commit', '-m', 'Initial commit'], { stdio: 'ignore' }],
+    ];
+
+    for (let command of commands) {
+      const result = spawn.sync(...command);
+      if (result.status !== 0) {
+        log.error(`Failed to run command ${command[0]} with ${command[1].join(' ')} options.`);
+        break;
+      }
+    }
+  }
+
+  async run() {
+    // Create the project directory.
+    this.createProjectDir();
+
+    // cd into the created project directory.
+    process.chdir(this.options.name);
+
+    // Clone the boilerplate repo.
+    await this.cloneRepo();
+
+    // Operations after creating project.
+    await this.postWrite();
+  }
+}
