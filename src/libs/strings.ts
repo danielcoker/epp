01ace5c4a98a1c51256c1216c20efbf442736959
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-12-09 05:28:50 +0100

    Add randomString function to strings lib

---
+++ src/libs/strings.ts
@@ -0,0 +1,12 @@
+const randomString = (len: number, charSet?: string) => {
+  charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+
+  for (let i = 0; i < len; i++) {
+    const randomPoz = Math.floor(Math.random() * charSet.length);
+    randomString += charSet.substring(randomPoz, randomPoz + 1);
+  }
+  return randomString;
+};
+
+export { randomString };
